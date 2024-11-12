2a824f288020748ce979f8faaa7f59211112d3f3
Andrea <16215183+andreamaestri@users.noreply.github.com>
2024-11-12 09:43:48 +0000

    Configure production settings and add Gunicorn for deployment to Heroku

--- core/settings.py
+++ core/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-1el&kjlwg3tm)l7wr+hc19q_1ik&2z#poc5ga&*g_y5niux_8+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
